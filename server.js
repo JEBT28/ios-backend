7911f907dbcfaa98b61df9b1a87895e693b950b4
Juan Esteban Baltierrez Tobon <juanbal2808@hotmail.com>
2022-05-18 10:24:00 -0500

    Entry point

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const server = express();
+
+server.get('/', (req, res) => {
+res.send('Hello World');
+});
+
+const port = process.env.PORT || 3000;
+
+server.listen(port)
+console.log("Escuchando Puerto: " + port);
